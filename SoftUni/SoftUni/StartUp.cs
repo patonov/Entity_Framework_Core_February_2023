43cad70af7f9cb50443ef479087bc53a47ee5305
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2023-02-25 22:29:17 +0200

    Add files via upload

---
+++ SoftUni/SoftUni/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using SoftUni.Data;
+
+namespace SoftUni
+    
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+            Console.WriteLine("Connection success!");
+        }
+    }
+}
