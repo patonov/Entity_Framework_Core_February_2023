ec71a58fdce596962fccc907aad9f0c52805ac40
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2023-03-04 13:20:37 +0200

    Add files via upload

---
+++ MusicHub/MusicHub/Data/Models/Performer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicHub.Data.Models
+{
+    public class Performer
+    {
+        public Performer() 
+        {
+            this.PerformerSongs = new HashSet<SongPerformer>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        [StringLength(20)]
+        public string LastName { get; set; } = null!;
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        public decimal NetWorth { get; set; }
+
+        public virtual ICollection<SongPerformer> PerformerSongs { get; set; }
+
+    }
+}
