50d89ad326f65052c70e93e84e9372df5cb5a244
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2023-03-19 21:45:39 +0200

    Add files via upload

---
+++ CarDealer/CarDealer/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            string inputSupplierString = File.ReadAllText(@"../../../Datasets/suppliers.json");
+
+
+            Console.WriteLine(ImportSuppliers(context, inputSupplierString));
+
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            IMapper mapper = new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CarDealerProfile>();
+            }));
+
+            ImportSuppliersDto[] suppliersDtos = JsonConvert.DeserializeObject<ImportSuppliersDto[]>(inputJson);
+            ICollection<Supplier> validSuppliers = new HashSet<Supplier>();
+
+            foreach (ImportSuppliersDto dto in suppliersDtos)
+            {
+                Supplier supplier = mapper.Map<Supplier>(dto);
+                validSuppliers.Add(supplier);            
+            }
+
+            context.Suppliers.AddRange(validSuppliers);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {validSuppliers.Count}.";
+
+        }
+    }
+}
