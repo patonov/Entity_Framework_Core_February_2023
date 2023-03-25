c5edfa80996f590bb67a787096665d4e807b9416
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2023-03-25 12:19:07 +0200

    Add files via upload

---
+++ CarDealer/CarDealer/CarDealerProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using CarDealer.DTOs.Export;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            this.CreateMap<ImportSuppliersDto, Supplier>();
+
+            this.CreateMap<ImportPartsDto, Part>();
+
+            this.CreateMap<ImportCarsDto, Car>();
+
+            this.CreateMap<ImportCustomersDto, Customer>();
+
+            this.CreateMap<ImportSalesDto, Sale>();
+
+            this.CreateMap<Car, ExportToyotaMakeDto>();
+        }
+    }
+}
