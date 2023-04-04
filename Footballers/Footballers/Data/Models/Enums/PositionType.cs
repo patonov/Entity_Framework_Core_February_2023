1031fe728a208289870edb12a4fdc0608c8e75bd
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2023-04-04 20:22:41 +0300

    Add files via upload

---
+++ Footballers/Footballers/Data/Models/Enums/PositionType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footballers.Data.Models.Enums
+{
+    public enum PositionType
+    {
+        Goalkeeper = 0, 
+        Defender = 1, 
+        Midfielder = 2, 
+        Forward = 3
+
+    }
+}
