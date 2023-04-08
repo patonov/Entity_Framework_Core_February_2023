f640f0b67c485ab7f40f64e0a7eabdfc00a57881
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2023-04-08 23:17:54 +0300

    Add files via upload

---
+++ Boardgames/Boardgames/Data/Models/Creator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Boardgames.Data.Models
+{
+    public class Creator
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(7)]
+        [MinLength(2)]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        [MaxLength(7)]
+        [MinLength(2)]
+        public string LastName { get; set; } = null!;
+
+       public virtual ICollection<Boardgame> Boardgames { get; set; } = new List<Boardgame>();
+
+    }
+}
