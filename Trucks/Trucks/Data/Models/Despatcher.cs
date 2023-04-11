fc492cb7fb308e4996f7fbdd0bad35f2163a05d0
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2023-04-11 23:02:05 +0300

    Add files via upload

---
+++ Trucks/Trucks/Data/Models/Despatcher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Trucks.Data.Models
+{
+    public class Despatcher
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        public string Name { get; set; } = null!;
+        
+        public string? Position { get; set; }
+
+        public virtual ICollection<Truck> Trucks { get; set; } = new List<Truck>();
+
+
+    }
+}
---
+++ Trucks/Trucks/Data/Models/Truck.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trucks.Data.Models.Enums;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Trucks.Data.Models
+{
+    public class Truck
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(8)]
+        [RegularExpression(@"[A-Z]{2}[0-9]{4}[A-Z]{2}")]
+        public string RegistrationNumber { get; set; } = null!;
+        
+        [Required]
+        [StringLength(17)]
+        public string VinNumber { get; set; } = null!;
+
+        [Range(950, 1420)]
+        public int TankCapacity { get; set; }
+
+        [Range(5000, 29000)]
+        public int CargoCapacity { get; set; }
+
+        [Required]
+        [Range(0, 3)]
+        public virtual CategoryType CategoryType { get; set; }
+
+        [Required]
+        [Range(0, 4)]
+        public MakeType MakeType { get; set; }
+
+        [ForeignKey(nameof(Despatcher))]
+        public int DespatcherId { get; set; }
+        public virtual Despatcher Despatcher { get; set; } = null!;
+
+        public virtual ICollection<ClientTruck> ClientsTrucks { get; set; } = new List<ClientTruck>();
+
+    }
+}
