7783d80e011187815cb2a4256c0dbbd0fa4ea23b
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2023-04-17 23:07:27 +0300

    Add files via upload

---
+++ Artillery/Artillery/DataProcessor/importdto/ImportGunDto.cs
@@ -0,0 +1,41 @@
+﻿using Artillery.Data.Models.Enums;
+using Artillery.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artillery.DataProcessor.ImportDto
+{
+    public class ImportGunDto
+    {
+        
+        public int ManufacturerId { get; set; }
+        
+        [Required]
+        [Range(100, 1350000)]
+        public int GunWeight { get; set; }
+
+        [Required]
+        [Range(2.00, 35.00)]
+        public double BarrelLength { get; set; }
+
+        public int? NumberBuild { get; set; }
+
+        [Required]
+        [Range(1, 100000)]
+        public int Range { get; set; }
+
+        [Required]
+        public string GunType { get; set; } = null!;
+                
+        public int ShellId { get; set; }
+
+        public ImportCountriesGunsDto[] Countries { get; set; } = null!;
+
+
+    }
+}
