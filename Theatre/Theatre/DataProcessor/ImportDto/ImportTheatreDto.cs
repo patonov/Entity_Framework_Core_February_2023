fd9301ee7d7ef41042e06fdfc4ee259f0eb0167e
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2023-04-25 18:56:25 +0300

    Add files via upload

---
+++ Theatre/Theatre/DataProcessor/ImportDto/ImportTheatreDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Theatre.Data.Models;
+
+namespace Theatre.DataProcessor.ImportDto
+{
+    public class ImportTheatreDto
+    {
+        [Required]
+        [MinLength(4)]
+        [MaxLength(30)]
+        public string Name { get; set; } = null!;
+
+        [Range(1, 10)]
+        public sbyte NumberOfHalls { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        [MaxLength(30)]
+        public string Director { get; set; } = null!;
+
+        public ImportTicketDto[] Tickets { get; set; }
+    }
+}
---
+++ Theatre/Theatre/DataProcessor/ImportDto/ImportTicketDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Theatre.Data.Models;
+
+namespace Theatre.DataProcessor.ImportDto
+{
+    public class ImportTicketDto
+    {
+        [Range(1.00, 100.00)]
+        public decimal Price { get; set; }
+
+        [Range(1, 10)]
+        public sbyte RowNumber { get; set; }
+
+        [Required]
+        public int PlayId { get; set; }
+
+        [Required]
+        public int TheatreId { get; set; }
+        
+
+
+    }
+}
