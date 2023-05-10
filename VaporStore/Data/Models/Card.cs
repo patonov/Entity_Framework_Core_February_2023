7201d8753973c4440659fde681abb22428c26158
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2023-05-10 18:49:25 +0300

    Add files via upload

---
+++ VaporStore/Data/Models/Card.cs
@@ -0,0 +1,38 @@
+﻿using Castle.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaporStore.Data.Models.Enums;
+
+namespace VaporStore.Data.Models
+{
+    public class Card
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"\d{4}[ ]\d{4}[ ]\d{4}[ ]\d{4}")]
+        public string Number { get; set; } = null!;
+
+        [Required]
+        [RegularExpression(@"\d{3}")]
+        public string Cvc { get; set; } = null!;
+
+        [Range(0, 1)]
+        public virtual CardType Type { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(User))]
+        public int UserId { get; set; }
+        public virtual User User { get; set; } = null!;
+
+       public virtual ICollection<Purchase> Purchases { get; set; } = new List<Purchase>();
+       
+
+    }
+}
---
+++ VaporStore/Data/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace VaporStore.Data.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        [MinLength(3)]
+        public string Username { get; set; } = null!;
+
+        [Required]
+        [RegularExpression(@"([A-Z][a-z]+)[ ]([A-Z][a-z]+)")]
+        public string FullName { get; set; } = null!;
+
+        [Required]
+        public string Email { get; set; } = null!;
+
+        [Range(3, 103)]
+        public int Age { get; set; }
+
+        public virtual ICollection<Card> Cards { get; set; } = new List<Card>();
+
+    }
+}
