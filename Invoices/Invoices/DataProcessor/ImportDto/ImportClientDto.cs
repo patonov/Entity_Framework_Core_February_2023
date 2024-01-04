01c76ee9df66dfaacda3b07325d1bdfd74d19ea9
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-01-04 23:51:21 +0200

    Add files via upload

---
+++ Invoices/Invoices/DataProcessor/ImportDto/ImportClientDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Invoices.DataProcessor.ImportDto
+{
+    [XmlType("Client")]
+    public class ImportClientDto
+    {
+        [Required]
+        [MaxLength(15)]
+        [MinLength(10)]
+        [XmlElement("Name")]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [MaxLength(15)]
+        [MinLength(10)]
+        [XmlElement("NumberVat")]
+        public string NumberVat { get; set; } = null!;
+
+        [XmlArray("Addresses")]
+        public AddressImportDto[] Addresses { get; set; } = null!;
+
+    }
+}
---
+++ Invoices/Invoices/DataProcessor/ImportDto/ImportProductDto.cs
@@ -0,0 +1,28 @@
+﻿using Invoices.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invoices.DataProcessor.ImportDto
+{
+    public class ImportProductDto
+    {
+        [Required]
+        [MaxLength(30)]
+        [MinLength(9)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [Range(5.00, 1000.00)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(0, 4)]
+        public virtual CategoryType CategoryType { get; set; }
+
+        public int[] GetInts { get; set; } = null!;
+    }
+}
