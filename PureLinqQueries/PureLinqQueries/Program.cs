64850eea9c653cf1d96e5b02084dd7841f8093fc
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-01-14 19:21:18 +0200

    Add files via upload

---
+++ PureLinqQueries/PureLinqQueries/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PureLinqQueries
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = [11, 12, 13, 14, 15];
+
+            IEnumerable<int> selectedNums =
+                from num in nums
+                where num > 13
+                select num;
+
+            foreach (var n in selectedNums)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
