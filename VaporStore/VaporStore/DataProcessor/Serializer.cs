4ba7c95e981a04108ae6dbc3dac77fa3a994e5da
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-02-22 21:20:56 +0200

    Add files via upload

--- VaporStore/VaporStore/DataProcessor/Serializer.cs
+++ VaporStore/VaporStore/DataProcessor/Serializer.cs
@@ -1,12 +1,46 @@
 ﻿namespace VaporStore.DataProcessor
 { 
     using Data;
+    using Newtonsoft.Json;
+    using VaporStore.Data.Models;
 
     public static class Serializer
     {
         public static string ExportGamesByGenres(VaporStoreDbContext context, string[] genreNames)
         {
-            throw new NotImplementedException();
+            var genres = context
+                .Genres
+                .ToArray()
+                .Where(g => genreNames.Contains(g.Name))
+                .Select(g => new
+                {
+                    Id = g.Id,
+                    Genre = g.Name,
+                    Games = g.Games
+                        .Where(ga => ga.Purchases.Any())
+                        .Select(ga => new
+                        {
+                            Id = ga.Id,
+                            Title = ga.Name,
+                            Developer = ga.Developer.Name,
+                            Tags = String.Join(", ", ga.GameTags
+                                .Select(gt => gt.Tag.Name)
+                                .ToArray()),
+                            Players = ga.Purchases.Count
+                        })
+                        .OrderByDescending(ga => ga.Players)
+                        .ThenBy(ga => ga.Id)
+                        .ToArray(),
+                    TotalPlayers = g.Games.Sum(ga => ga.Purchases.Count)
+                })
+                .OrderByDescending(g => g.TotalPlayers)
+                .ThenBy(g => g.Id)
+                .ToArray();
+
+            string json = JsonConvert.SerializeObject(genres, Formatting.Indented);
+
+            return json;
+
         }
 
         public static string ExportUserPurchasesByType(VaporStoreDbContext context, string purchaseType)
