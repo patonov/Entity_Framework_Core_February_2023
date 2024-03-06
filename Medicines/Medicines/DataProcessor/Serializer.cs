8844a6c6c2312d10577924568b1c53d2c2f3b928
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-03-06 22:00:59 +0200

    Add files via upload

--- Medicines/Medicines/DataProcessor/Serializer.cs
+++ Medicines/Medicines/DataProcessor/Serializer.cs
@@ -3,15 +3,55 @@
     using Medicines.Data;
     using Medicines.Data.Models;
     using Medicines.Data.Models.Enums;
+    using Medicines.DataProcessor.ExportDtos;
     using Microsoft.EntityFrameworkCore;
     using Newtonsoft.Json;
     using System.Diagnostics;
+    using System.Globalization;
+    using System.IdentityModel.Tokens.Jwt;
+    using System.Text;
+    using System.Xml.Serialization;
 
     public class Serializer
     {
         public static string ExportPatientsWithTheirMedicines(MedicinesContext context, string date)
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ExportPatientDto[]), new XmlRootAttribute("Patients"));
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            using StringWriter stringWriter = new StringWriter(sb);
+
+            DateTime dateTime = DateTime.ParseExact(date, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+
+            var patients = context.Patients.ToArray()
+                .Where(p => p.PatientsMedicines.Any(p => p.Medicine.ProductionDate >= dateTime))
+                .Select(p => new ExportPatientDto() 
+                { 
+                Name = p.FullName,
+                Gender = p.Gender.ToString(),
+                AgeGroup = p.AgeGroup.ToString(),
+                Medicines = context.Medicines.ToArray()
+                .Where(m => m.ProductionDate >= dateTime).OrderByDescending(m => m.ExpiryDate).ThenBy(m => m.Price)
+                .Select(m => new ExportMedicinesDto() 
+                    { 
+                    Name = m.Name,
+                    Price = m.Price.ToString("F2"),
+                    Producer = m.Producer,
+                    BestBefore = m.ExpiryDate.ToString("yyyy-MM-dd"),
+                    Category = m.Category.ToString()
+                    }).ToArray()
+                }).OrderByDescending(p => p.Medicines.Length)
+                .ThenBy(p => p.Name)
+            .ToArray();
+
+            xmlSerializer.Serialize(stringWriter, patients, namespaces);
+
+            return sb.ToString().Trim();
+
+
         }
 
         public static string ExportMedicinesFromDesiredCategoryInNonStopPharmacies(MedicinesContext context, int medicineCategory)
