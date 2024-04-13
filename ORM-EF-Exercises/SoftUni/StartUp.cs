01ece22f35bb30f29437578682c0a192fcc5e0a1
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-04-13 20:24:25 +0300

    Add files via upload

--- ORM-EF-Exercises/SoftUni/StartUp.cs
+++ ORM-EF-Exercises/SoftUni/StartUp.cs
@@ -9,9 +9,10 @@ namespace SoftUni
         static void Main()
         {
             var dbContext = new SoftUniContext();
-            
+
             //Console.WriteLine(GetEmployeesFullInformation(dbContext));
-            Console.WriteLine(GetEmployeesWithSalaryOver50000(dbContext));
+            //Console.WriteLine(GetEmployeesWithSalaryOver50000(dbContext));
+            Console.WriteLine(GetEmployeesFromResearchAndDevelopment(dbContext));
         }
 
         //Problem 01
@@ -64,6 +65,22 @@ namespace SoftUni
         public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
         {
             StringBuilder sb = new StringBuilder();
+            var employees = context.Employees.Where(e => e.Department.Name == "Research and Development")
+                .Select(e => new 
+                { 
+                e.FirstName,
+                e.LastName,
+                DepartmentName = e.Department.Name,
+                e.Salary
+                }).OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName)
+                .ToArray();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} from {employee.DepartmentName} - ${employee.Salary:f2}");
+            }
+
             return sb.ToString().Trim();
         }
 
