d0fc41c52f0205bb52173e0044c06f589f974d68
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-04-24 16:10:57 +0300

    Add files via upload

---
+++ 03. ORM-EF-Lab-Resources/SoftUni/StartUp.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {        
+        static void Main()
+        {
+            var context = new SoftUniContext();
+
+            //Console.WriteLine(DeleteFirstProject(context));
+
+            Console.WriteLine(UpdateAddresses(context));
+        }
+
+        
+        //Problem 07
+        public static string DeleteFirstProject(SoftUniContext context)
+        {
+            Project project = context.Projects.FirstOrDefault();
+
+            if (project != null)
+            {
+                context.Projects.Remove(project);
+                context.SaveChanges();
+                return project.Name;
+            }
+
+            return "";
+        }
+
+        //Problem 08
+        public static string UpdateAddresses(SoftUniContext context)
+        {
+            var addresses = context.Addresses
+                .Where(a => a.AddressText.Contains("Drive")).ToList();
+
+            foreach (var a in addresses)
+            {
+                a.TownId = 2;
+            }
+
+            context.SaveChanges();
+
+            return addresses.Count.ToString();
+        }
+    }
+}
