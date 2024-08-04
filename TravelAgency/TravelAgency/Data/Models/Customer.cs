570d3ef2113f0ee0d3f0c3cbe6a5693d46d83e01
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-08-04 22:34:44 +0300

    Add files via upload

---
+++ TravelAgency/TravelAgency/Data/Models/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgency.Data.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        [MaxLength(60)]
+        public string FullName { get; set; } = null!;
+
+        [Required]
+        [MinLength(6)]
+        [MaxLength(50)]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [RegularExpression("(\\+[0-9]{12})")]
+        public string PhoneNumber { get; set; } = null!;
+
+        public virtual ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+    }
+}
---
+++ TravelAgency/TravelAgency/Data/Models/Guide.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAgency.Data.Models.Enums;
+
+namespace TravelAgency.Data.Models
+{
+    public class Guide
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        [MaxLength(60)]
+        public string FullName { get; set; } = null!;
+
+        [Required]
+        public virtual Language Language { get; set; }
+
+        public virtual ICollection<TourPackageGuide> TourPackagesGuides { get; set; } = new List<TourPackageGuide>();
+
+
+    }
+}
