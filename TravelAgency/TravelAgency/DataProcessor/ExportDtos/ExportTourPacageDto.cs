ee19419af822a28e3972c0fca225d8a456bbd318
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-08-06 16:13:47 +0300

    Add files via upload

---
+++ TravelAgency/TravelAgency/DataProcessor/ExportDtos/ExportTourPacageDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using TravelAgency.Data.Models;
+
+namespace TravelAgency.DataProcessor.ExportDtos
+{
+    [XmlType("TourPackage")]
+    public class ExportTourPacageDto
+    {
+        [XmlElement("Name")]
+        public string Name { get; set; } = null!;
+
+        [XmlElement("Description")]
+        public string? Description { get; set; }
+
+        [XmlElement("Price")]
+        public decimal Price { get; set; }
+
+    }
+}
--- TravelAgency/TravelAgency/DataProcessor/Serializer.cs
+++ TravelAgency/TravelAgency/DataProcessor/Serializer.cs
@@ -1,4 +1,11 @@
-﻿using TravelAgency.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Text;
+using System.Xml.Serialization;
+using TravelAgency.Data;
+using TravelAgency.Data.Models;
+using TravelAgency.Data.Models.Enums;
+using TravelAgency.DataProcessor.ExportDtos;
 
 namespace TravelAgency.DataProcessor
 {
@@ -6,12 +13,54 @@ namespace TravelAgency.DataProcessor
     {
         public static string ExportGuidesWithSpanishLanguageWithAllTheirTourPackages(TravelAgencyContext context)
         {
-            throw new NotImplementedException();
+            ExportGuideDto[] guideDtos = context.Guides.Where(g => (int)g.Language == 3)
+                .OrderByDescending(g => g.TourPackagesGuides.Count).ThenBy(g => g.FullName)
+                .Select(g => new ExportGuideDto 
+                { 
+                FullName = g.FullName,
+                TourPackages = g.TourPackagesGuides.Select(tp => tp.TourPackage)
+                    .OrderByDescending(tp => tp.Price)
+                    .ThenBy(tp => tp.PackageName)
+                    .Select(tp => new ExportTourPacageDto 
+                        { 
+                        Name = tp.PackageName,
+                        Description = tp.Description,
+                        Price = tp.Price
+                        }).ToArray()
+                })
+                .ToArray();
+
+            StringBuilder sb = new StringBuilder();
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ExportGuideDto[]), new XmlRootAttribute("Guides"));
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            using StringWriter writer = new StringWriter(sb);
+            xmlSerializer.Serialize(writer, guideDtos, namespaces);
+
+            return sb.ToString().TrimEnd();
         }
 
         public static string ExportCustomersThatHaveBookedHorseRidingTourPackage(TravelAgencyContext context)
         {
-            throw new NotImplementedException();
+            var customers = context.Customers.ToArray()
+                .Where(c => c.Bookings.Any(b => b.TourPackage.PackageName == "Horse Riding Tour"))
+                .OrderBy(c => c.FullName)
+                .ThenBy(c => c.Bookings.Count())
+                .Select(c => new
+                {
+                    c.FullName,
+                    c.PhoneNumber,
+                    Bookings = c.Bookings.Where(b => b.TourPackage.PackageName == "Horse Riding Tour")
+                        .Select(b => new
+                        {
+                            TourPackageName = b.TourPackage.PackageName,
+                            Date = b.BookingDate.ToString("yyyy-MM-dd")
+                        }).OrderBy(b => b.Date)
+                });
+
+            return JsonConvert.SerializeObject(customers, Formatting.Indented);
         }
     }
 }
